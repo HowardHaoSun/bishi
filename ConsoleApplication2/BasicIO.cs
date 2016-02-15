3bfa17321e8abf094bcc5b2de7c74b4cb6ec1a61
HowardHaoSun <sunhaocmdi@126.com>
2016-02-15 14:44:06 +0800

    新建Kiwibank笔试

---
+++ ConsoleApplication2/BasicIO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    public abstract class BasicIO
+    {
+        public abstract string GetCurrentDictionary();
+
+        public abstract void Output(string input);
+
+        public abstract string Input();
+
+        public abstract void Excute();
+        public abstract void SetCurDirectory(string input);
+       
+    }
+}
---
+++ ConsoleApplication2/Dir.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApplication2
+{
+    public class Dir:BasicIO
+    {
+        public override string Input()
+        {
+            return Console.ReadLine();
+        }
+        public override void Output(string input)
+        {
+            Console.WriteLine(input);
+        }
+        public override string GetCurrentDictionary()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+        public override void SetCurDirectory(string input)
+        {
+            Directory.SetCurrentDirectory(input);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+        public override void Excute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
